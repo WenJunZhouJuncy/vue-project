8eda1ab2eaf17a69153a2be5b7fbbeb5c1689d4a
WenJunZhouJuncy <980497826@qq.com>
2019-12-10 23:19:29 +0800

    请求前头部信息 成功登录

--- src/assets/js/request.js
+++ src/assets/js/request.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
-import { Message } from 'element-ui';
 
 const BASEURL = process.env.NODE_ENV === 'production' ? '' : '/devApi';
 
 const api = axios.create({//创建拦截器
     baseURL:BASEURL,         //   地址/devApi   == "http://www.web-jshtml.cn/productapi"
-    timeout:15000,      //15秒超时
+    timeout:15000,      //请求过程15秒超时
 });
 
 // 添加请求拦截器
 api.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
+    // 添加头部信息
+    // config.headers.Tokey = '11111';
+    // config.headers.userId = '22222';
+    // console.log(config.headers);
+
     return config;
 }, function (error) {
     // 对请求错误做些什么
@@ -22,10 +26,9 @@ api.interceptors.response.use(function (response) {
     // 对响应数据做点什么  response请求成功返回的数据
     let data = response.data;
     if (data.resCode !== 0){
-        Message.error(data.message);
         return Promise.reject(data);
     }else {
-        return response;
+        return Promise.resolve(data);
     }
 }, function (error) {
     // 对响应错误做点什么
